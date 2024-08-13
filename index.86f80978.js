c87e43cebe2a272b7f27bc16ab2946938df0f56d
unknown <oleksandr.plaksin25@gmail.com>
2024-08-13 13:28:51 +0200

    make build

---
+++ index.86f80978.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item").querySelector("a");e.src=r.href});
+//# sourceMappingURL=index.86f80978.js.map
